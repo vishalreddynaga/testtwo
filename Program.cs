a6924adf2d9b721604abef20ac533ad17b174bef
vishalreddynaga <Vishalreddyn1@>
2021-03-10 10:41:12 -0800

    tes

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace temp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hegibi)))))))))))))))))))))))))))))))rld!");
+
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now.ToString("F"));
+        }
+    }
+}
